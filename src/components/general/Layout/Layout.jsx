e384facd0b1f3cefd10b409b5ef1ac11b4183646
twixer554 <twixer554@gmail.com>
2023-02-25 19:40:28 +0500

    Layout update

--- src/components/general/Layout/Layout.jsx
+++ src/components/general/Layout/Layout.jsx
@@ -12,6 +12,8 @@ export default function Layout({ children, pageClass }) {
   const dispatch = useDispatch()
   const user = useSelector((state) => state.user.value)
 
+  console.log(2)
+
   React.useEffect(() => {
     api.auth
       .check()
