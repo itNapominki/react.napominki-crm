299c8e8c96fdc83efa777e6d10bfa1ad2cc68aa7
twixer554 <twixer554@gmail.com>
2023-03-29 12:52:50 +0500

    manager map develope

--- src/pages/client/OfferPage/components/Map/Map.jsx
+++ src/pages/client/OfferPage/components/Map/Map.jsx
@@ -3,50 +3,54 @@ import React from 'react'
 import ymaps from 'ymaps'
 
 export default function Map({ points }) {
-  const center = points
-    ?.reduce((prev, next) => [
-      prev.coordinates[0] + next.coordinates[0],
-      prev.coordinates[1] + next.coordinates[1],
-    ])
-    .map((coordinate) => coordinate / points.length)
+  const center =
+    points.length === 1
+      ? points[0].coordinates
+      : points
+          ?.reduce((prev, next) => [
+            prev.coordinates[0] + next.coordinates[0],
+            prev.coordinates[1] + next.coordinates[1],
+          ])
+          .map((coordinate) => coordinate / points.length)
 
-  React.useEffect(() => {
-    ymaps
-      .load()
-      .then((maps) => {
-        document.getElementById('map').innerHTML = ''
+  ymaps
+    .load()
+    .then((maps) => {
+      document.getElementById('map').innerHTML = ''
 
-        const map = new maps.Map('map', {
-          center,
-          zoom: 12,
-        })
-
-        const objectManager = new maps.ObjectManager()
-
-        objectManager.add(
-          points.map(({ id, coordinates }) => ({
-            type: 'Feature',
-            id,
-            geometry: {
-              type: 'Point',
-              coordinates,
-            },
-            options: {
-              iconLayout: 'default#image',
-              iconImageHref: '/marker-cafe.svg',
-              iconImageSize: [46, 60],
-              iconImageOffset: [-20, -60],
-            },
-          }))
-        )
-
-        map.geoObjects.add(objectManager)
-        map.setBounds(map.geoObjects.getBounds())
-        map.margin.setDefaultMargin(50)
+      const map = new maps.Map('map', {
+        center,
+        zoom: 12,
       })
-      .then(() => console.log('map ready'))
-      .catch((error) => console.log('Failed to load Yandex Maps', error))
-  }, [])
+
+      const objectManager = new maps.ObjectManager()
+
+      objectManager.add(
+        points.map(({ id, coordinates }) => ({
+          type: 'Feature',
+          id,
+          geometry: {
+            type: 'Point',
+            coordinates,
+          },
+          options: {
+            iconLayout: 'default#image',
+            iconImageHref: '/marker-cafe.svg',
+            iconImageSize: [46, 60],
+            iconImageOffset: [-20, -60],
+          },
+        }))
+      )
+
+      map.geoObjects.add(objectManager)
+
+      if (points.length > 1) {
+        map.setBounds(map.geoObjects.getBounds())
+        map.margin.setDefaultMargin(100)
+      }
+    })
+    .then(() => console.log('map ready'))
+    .catch((error) => console.log('Failed to load Yandex Maps', error))
 
   return <div id="map" className={styles.container}></div>
 }
