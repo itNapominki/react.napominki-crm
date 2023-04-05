b131d5549ef967e8af61b24ce4cb862436955c5a
twixer554 <twixer554@gmail.com>
2023-04-05 16:11:13 +0500

    manager map develope

--- src/pages/client/RestaurantPage/components/Offer/Offer.jsx
+++ src/pages/client/RestaurantPage/components/Offer/Offer.jsx
@@ -5,9 +5,11 @@ import { Messengers } from './'
 
 export default function Offer({ card = true, title, messengers }) {
   return (
-    <div className={card ? styles.card : null}>
-      {title && <div className={styles.title}>{title}</div>}
-      <Messengers messengers={messengers} />
-    </div>
+    messengers && (
+      <div className={card ? styles.card : null}>
+        {title && <div className={styles.title}>{title}</div>}
+        <Messengers messengers={messengers} />
+      </div>
+    )
   )
 }
