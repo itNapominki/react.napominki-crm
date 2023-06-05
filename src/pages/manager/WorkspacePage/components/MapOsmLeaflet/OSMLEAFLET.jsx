33e879ef76e075b8a520c01452b3d2f4fd0bf9a2
“itNapominki” <it.napominki.mos@gmail.com>
2023-06-05 13:29:24 +0300

    fix: убрал лишний код

--- src/pages/manager/WorkspacePage/components/MapOsmLeaflet/OSMLEAFLET.jsx
+++ src/pages/manager/WorkspacePage/components/MapOsmLeaflet/OSMLEAFLET.jsx
@@ -117,7 +117,7 @@ export default function OSMLEAFLET({
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />{" "}
+        />
         <MapEvents />
         {circle}
         {selectPosition && (
